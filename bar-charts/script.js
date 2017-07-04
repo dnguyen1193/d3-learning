446be6375651330868cf930f24198ce9f4bbc837
dnguyen <dnguyen@broadinstitute.org>
2017-07-03 23:42:30 -0400

    copying over code to use as template for rotating SVG bar chart

--- bar-charts/script.js
+++ bar-charts/script.js
@@ -35,6 +35,40 @@
 //     .text(function(d) {return d;});
 
 // code for static SVG bar chart w/ data load
+// var totalWidth = 420,
+//     barWidth = 20;
+// var x = d3.scaleLinear()
+//           .range([0, totalWidth]);
+// var svg = d3.select('.chart')
+//             .attr('width', totalWidth);
+// d3.tsv('data.tsv', type, function(err, data) {
+//     console.log(data);
+//     x.domain([0, d3.max(data, function(d) { return d.value; })]);
+//     svg.attr('height', barWidth*data.length);
+//     var bars = svg.selectAll('g')
+//                     .data(data)
+//                   .enter().append('g')
+//                     .attr('transform',
+//                           function(d, i) {
+//                             return 'translate(0,'
+//                                     + i*20 +')';
+//                           });
+//     bars.append('rect')
+//         .attr('height', barWidth - 1)
+//         .attr('width', function(d, i) { return x(d.value); });
+//     bars.append('text')
+//         .style('dy', '.35em')
+//         .attr('y', 12.5)
+//         .attr('x', function(d) { return x(d.value) - 2; })
+//         .text(function(d) { return d.value; });
+// });
+
+// function type(d) {
+//     d.value = +d.value;
+//     return d;
+// }
+
+// code for rotated static SVG bar chart w/ data load
 var totalWidth = 420,
     barWidth = 20;
 var x = d3.scaleLinear()
